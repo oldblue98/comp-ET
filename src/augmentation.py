b98f7c62d330c269af0a791039588abac7236c0b
oldblue98 <takahashikeiji@takahashikyoujinoMacBook-Air.local>
2021-08-14 17:33:57 +0900

    modefied utils.py

--- src/augmentation.py
+++ src/augmentation.py
@@ -32,8 +32,8 @@ def get_train_transforms(input_shape):
             VerticalFlip(p=0.5),
             ShiftScaleRotate(p=0.5),
             Normalize(
-                mean=[0.485, 0.456, 0.406],
-                std=[0.229, 0.224, 0.225],
+                mean=[0.485],
+                std=[0.229],
             ),
             ToTensorV2(p=1.0),
         ], p=1.)
@@ -42,8 +42,8 @@ def get_valid_transforms(input_shape):
     return Compose([
                 Resize(input_shape, input_shape),
                 Normalize(
-                mean=[0.485, 0.456, 0.406],
-                std=[0.229, 0.224, 0.225],
+                mean=[0.485],
+                std=[0.229],
             ),
                 ToTensorV2(p=1.0),
             ], p=1.)
