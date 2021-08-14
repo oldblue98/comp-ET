f35c07f784e020828578f3d4add5b935e450c06f
oldblue98 <takahashikeiji@takahashikyoujinoMacBook-Air.local>
2021-08-14 20:22:27 +0900

    modefied

--- src/model.py
+++ src/model.py
@@ -74,7 +74,7 @@ class ImageModel(nn.Module):
         super(ImageModel,self).__init__()
         print('Building Model Backbone for {} model'.format(model_name))
 
-        self.backbone = timm.create_model(model_name, num_classes=0, pretrained=pretrained, in_chans=in_channels)
+        self.backbone = timm.create_model(model_name, num_classes=0, pretrained=pretrained, in_chans=in_channels, num_classes=n_classes)
         self.model_type = model_type
         in_features = self.backbone.num_features
         print(f"{model_name}: {in_features}, fc_dim :{fc_dim}")
@@ -120,7 +120,7 @@ class ImageModel(nn.Module):
         nn.init.constant_(self.bn.bias, 0)
 
     def forward(self, image):
-        feature = self.extract_feat(image)
+        feature = self.backbone(image)
         return feature
 
     def extract_feat(self, x):
