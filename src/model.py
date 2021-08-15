61fd3f36cb5ff51d5d0c72d029a5591e5c898b90
oldblue98 <takahashikeiji@takahashikyoujinoMacBook-Air.local>
2021-08-16 03:00:37 +0900

    modefied model.py

--- src/model.py
+++ src/model.py
@@ -94,7 +94,7 @@ class ImageModel(nn.Module):
             nb_ft = self.backbone.last_linear.in_features
             self.backbone.last_linear = nn.Identity()
         elif hasattr(self.backbone, "head"):
-            nb_ft = self.backbone.num_features
+            nb_ft = self.backbone.head.fc.in_features
             self.backbone.head = nn.Identity()
         print("nb_ft : ", nb_ft)
         self.block1 = nn.Sequential(
